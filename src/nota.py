5e529a84e361d7a42b9f257101143ff3c2c46b33
Paszser <dbpaszser@gmail.com>
2021-10-14 09:14:16 +0200

    Borra comentarios en código y avanza #16 y #17

--- src/nota.py
+++ src/nota.py
@@ -1,22 +1,18 @@
 from dataclasses import dataclass
 
-#Clase Nota
 @dataclass(frozen=True)
 class Nota:
-    contenido = '' #Contenido de la nota
-    cajon = '' #Cajón al que pertenece la nota
+    contenido = ''
+    cajon = ''
 
-    #Contructor de la clase Cajón
     def _init_(self, contenido, cajon):
         self.cajon = cajon
 
-    #Getters de los atributos de la clase Cajón
     def getContenido(self):
         return self.contenido
 
     def getCajon(self):
         return self.cajon
 
-    #Método para clasificar la nota en un cajón
     def clasificarNota(contenido):
         #Implementación
