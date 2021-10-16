2ab358655e7ad3985fb7c9ecac177e233b84d7cd
Paszser <dbpaszser@gmail.com>
2021-10-16 13:34:37 +0200

    Elimina dataclass y añade los atributos que se comentan en #20 avanzándolo. Avanza #18 y #19 a su vez.

--- src/nota.py
+++ src/nota.py
@@ -1,17 +1,18 @@
-from dataclasses import dataclass
-
-@dataclass(frozen=True)
 class Nota:
-    contenido = ''
-    cajon = ''
+    """ Clase que define una nota que contiene un texto y una serie de palabras clave que la identifican """
+    texto = ''
+    palabrasClave = []
 
-    def _init_(self, contenido, cajon):
-        self.cajon = cajon
+    def _init_(self, texto):
+        self.texto = texto
+        """Constructor de la clase que se pasa por parámetro el texto de una nota"""
 
-    def getContenido(self):
-        return self.contenido
+    def getTexto(self):
+        return self.texto
+        """ Método Get para el texto de la nota"""
 
-    def getCajon(self):
-        return self.cajon
+    def getPalabrasClave(self):
+        return self.palabrasClave
+        """Método Get para las palabras clave de la nota"""
 
     
