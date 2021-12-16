275a0411f853d66b9a707dced47335309aa21cbe
Jose Manu <jmgl.rm@gmail.com>
2021-12-16 16:49:18 +0100

    Creado sistema de configuración. closes #32

---
+++ cappjon/config.py
@@ -0,0 +1,24 @@
+import os
+from dotenv import load_dotenv
+
+class Config: 
+
+	def __init__(self):
+		load_dotenv()
+		if (os.getenv('LOG_DIR'):
+			self.log_dir = os.getenv('LOG_DIR')
+		else:
+			self.log_dir = '/logs/'
+
+		if (os.getenv('LOG_FILE'):
+			self.log_file = os.getenv('LOG_FILE')
+		else:
+			self.log_file = 'notas.log'
+
+
+	def get_directorio(self):
+		return self.log_dir
+
+	def get_archivo(self):
+		return self.log_file
+
