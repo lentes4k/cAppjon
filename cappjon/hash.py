3319338dd2f9e36baa585d360a7ed5a90122c91d
Jose Manu <jmgl.rm@gmail.com>
2021-12-19 12:42:27 +0100

    fix

--- cappjon/hash.py
+++ cappjon/hash.py
@@ -1,7 +1,9 @@
 class Hash
 
-	def __init__(self, valor):
+	def __init__(self):
 		key = 0
+
+	def hashing(self, valor):
 		for i in range(0, len(valor)):
 			key+= ord(valor[i])
 		return key % 127 
