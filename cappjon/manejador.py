1ba59776e70a1ebeadc46f366a8588c71f4ccdd0
Jose Manu <jmgl.rm@gmail.com>
2021-12-22 19:59:52 +0100

    Añado mensaje de logging

--- cappjon/manejador.py
+++ cappjon/manejador.py
@@ -2,18 +2,21 @@ import logging
 
 from .nota import Nota
 from .cappjon import Cappjon
+from .logger import Logger_notas
 
 class Manejador:
 
 	def __init__ (self):
 		self.nota : Nota
 		self.cappjon : Cappjon
+		self.logger : Logger
 
 	def crea_nota(self, texto):
 		try:
 			self.cappjon = Cappjon("Cajon de prueba")
 			self.nota = Nota(texto)
 			self.nota.almacenar
+			self.logger.info("La nota ha sido creado y añadida al cappjón"
 		except Exception as excepcion:
 			logging.error('Nota no creada')
 			raise excepcion
