c5cc6b73b53673028d70a7b18c5b787116996a75
Jose Manu <jmgl.rm@gmail.com>
2021-12-27 10:58:14 +0100

    Añado diferentes tipos de log

--- cappjon/logger.py
+++ cappjon/logger.py
@@ -11,6 +11,19 @@ class Logger_notas:
 		directorio = configuration.get_directorio()
 		archivo = configuration.get_archivo()
 		path = directorio + archivo
-
-	def logging(self):
 		logging.basicConfig(filename=path, format='%(asctime)s %(message)s')
+
+	def info(self, msg):
+		logging.getLogger().info(msg)
+
+	def error(self, msg):
+		logging.getLogger().error(msg)
+
+	def debug(self, msg):
+		logging.getLogger().debug(msg)
+
+	def warning(self, msg):
+		logging.getLogger().warning(msg)
+
+	def critical(self, msg):
+		logging.getLogger().critical(msg)
